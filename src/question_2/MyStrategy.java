8f6fc34ad274bb3e5300ad119581893097b0ca9f
jakeabeany <jakemcvey1@gmail.com>
2017-01-31 18:11:44 +0000

    added strategyFactory and MYStrategy

---
+++ src/question_2/MyStrategy.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package question_2;
+
+/**
+ *
+ * @author Jake McVey
+ */
+public class MyStrategy implements Strategy{
+
+    @Override
+    public boolean cheat(Bid b, Hand h) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Bid chooseBid(Bid b, Hand h, boolean cheat) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean callCheat(Hand h, Bid b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
